8a4bd11f1733aa0cf83241b52026111121818358
Diego Ricardo <diegoricardoweb@gmail.com>
2021-08-19 12:55:39 -0300

    Resolução exercicio 2 dia 3

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,17 +1,5 @@
 const nome = document.querySelector('[data-js="nome"]')
-
-// const convertWords = (e) => {
-//   return (e).toLowerCase()
-//             .split(' ')
-//             .map((w) => {
-//               const a = w[0].toUpperCase() + w.slice(1)
-
-//               return a.replace('De', 'de')
-//                       .replace('Da', 'da')
-//                       .replace('Do', 'do')
-//                       .replace('Dos', 'dos')
-//             }).join(' ')
-// }
+const form = document.querySelector('[data-js="form"]')
 
 function convertWords(str) {
   return str.replace(
@@ -30,4 +18,49 @@ nome.addEventListener('input', (e) => {
   field.value = convertWords(e.target.value)
 })
 
+//exercicio 2
+
+let sel = document.createElement("select");
+sel.setAttribute('multiple', 'multiple')
+sel.setAttribute('data-js', 'colors')
+
+let optarr =  ['Blue','Green','Yellow','Black','Red'];
+for(var i = 0;i<optarr.length;i++) {
+  let opt = document.createElement("option");
+  opt.text = optarr[i];
+  opt.value = optarr[i].toLowerCase();
+  opt.className = optarr[i];
+  sel.appendChild(opt);
+}
+
+form.appendChild(sel);
+
+const colors = document.querySelector('[data-js="colors"]')
+const boxColors = document.createElement("div");
+boxColors.setAttribute('data-js', 'Boxcolors')
+boxColors.setAttribute('style', 'display: flex; justify-content: center;')
+
+form.insertAdjacentHTML('afterend', boxColors.outerHTML);
+
+const removeDivs = (element) => {
+  element.forEach((el) => {
+    el.remove()
+  })
+}
+
+colors.addEventListener('change', (e) => {
+  const selecteds = [...e.target.selectedOptions].map(el => el.value)
+  const boxDivColors = document.querySelector('[data-js="Boxcolors"]')
+  const divCriadas = document.querySelectorAll('[data-js="divColors"]')
+
+  removeDivs(divCriadas)
+
+  selecteds.map((div) => {
+    let boxDiv = document.createElement("div");
+    boxDiv.setAttribute('style', `width: 100px; height: 100px; background: ${div}`)
+    boxDiv.setAttribute('data-js', 'divColors')
+
+    boxDivColors.appendChild(boxDiv)
+  })
+})
 
