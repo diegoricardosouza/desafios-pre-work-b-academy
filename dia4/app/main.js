b0fb1554579df6a99526d337ddd809d624d24512
Diego Ricardo <diegoricardoweb@gmail.com>
2021-08-20 12:57:54 -0300

    Resolução exercicio 3 dia 4

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -2,22 +2,21 @@ import './style.css'
 
 const url = 'http://localhost:3333/cars';
 const tabelaBody = document.querySelector('[data-js="tabelaBody"]')
+const formCarro = document.querySelector('[data-js="formCarro"]')
+const msg = document.querySelector('[data-js="msg"]')
 
-fetch(url).then(function(response) {
-  response.json().then(function(data) {
-    populaTable(data);
+fetch(url).then((response) => {
+  response.json().then((data) => {
+    populaTable(data)
   });
 })
 
 function populaTable(data) {
-  const tr = document.createElement("tr")
-  tabelaBody.appendChild(tr)
-
   if (data.length > 0) {
-    data.map((input) => {
-      const td = document.createElement("td");
-      td.textContent = input
-      tr.appendChild(td)
+    data.forEach((input) => {
+      const a = [input.image, input.brandModel, input.year, input.plate, input.color]
+
+      createLines(a)
     })
   } else {
     const td = document.createElement("td");
@@ -25,5 +24,81 @@ function populaTable(data) {
     td.setAttribute('colspan', '5')
     tr.appendChild(td)
   }
-
 }
+
+function createNewTable(res) {
+  msg.innerHTML = ''
+  const div = document.createElement("div")
+  div.textContent = res.message
+  div.classList.add("success");
+  msg.appendChild(div)
+
+  fetch(url)
+  .then(result => result.json())
+  .then((result) => {
+    tabelaBody.innerHTML = ''
+
+    result.map((res) => {
+      const a = [res.image, res.brandModel, res.year, res.plate, res.color]
+      createLines(a)
+    })
+  })
+}
+
+function insertData (data) {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then((res) => {
+      res.json()
+      .then((res) => {
+        if(!!res.error) {
+          msg.innerHTML = ''
+          const div = document.createElement("div")
+          div.textContent = res.message
+          msg.appendChild(div)
+        } else {
+          createNewTable(res)
+        }
+      })
+  })
+}
+
+function createLines(arr) {
+  const tr = document.createElement("tr")
+  tabelaBody.appendChild(tr)
+
+  arr.forEach((res) => {
+    const td = document.createElement("td")
+    td.textContent = res
+    tr.appendChild(td)
+  })
+}
+
+formCarro.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const imagem = e.target.elements.img.value
+  const marca = e.target.elements.marca.value
+  const ano = e.target.elements.ano.value
+  const placa = e.target.elements.placa.value
+  const cor = e.target.elements.cor.value
+
+  const data = {
+    image: imagem,
+    brandModel: marca,
+    year: ano,
+    plate: placa,
+    color: cor
+  }
+
+  insertData(data)
+
+  formCarro.reset()
+  e.target.elements.img.focus()
+})
+
+
