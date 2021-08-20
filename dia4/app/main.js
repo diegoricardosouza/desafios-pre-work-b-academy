c9f7559ee1ae10b4c96d27e8684fcaea086256f9
Diego Ricardo <diegoricardoweb@gmail.com>
2021-08-20 10:11:12 -0300

    Resolução exercicio 2 dia 4

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,3 +1,29 @@
 import './style.css'
 
+const url = 'http://localhost:3333/cars';
+const tabelaBody = document.querySelector('[data-js="tabelaBody"]')
 
+fetch(url).then(function(response) {
+  response.json().then(function(data) {
+    populaTable(data);
+  });
+})
+
+function populaTable(data) {
+  const tr = document.createElement("tr")
+  tabelaBody.appendChild(tr)
+
+  if (data.length > 0) {
+    data.map((input) => {
+      const td = document.createElement("td");
+      td.textContent = input
+      tr.appendChild(td)
+    })
+  } else {
+    const td = document.createElement("td");
+    td.textContent = "Nenhum carro encontrado"
+    td.setAttribute('colspan', '5')
+    tr.appendChild(td)
+  }
+
+}
